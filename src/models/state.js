96ee535b4348a5a03771b7b24e21c44f7d9d0414
Krzysztof Mędrzycki <aiwenari@gmail.com>
2018-04-26 12:29:02 +0200

    Add State

---
+++ src/models/state.js
@@ -0,0 +1,41 @@
+import { Record, Map, List, Seq } from 'immutable'
+
+const DEFAULTS = {
+    key: null,
+    type: null,
+    counters: new Map(),
+    nodes: new List(),
+}
+
+/**
+ * State of counters in a single document sub-tree.
+ *
+ * @property {string} key - key of the node this state belongs to.
+ *
+ * @property {string|null} type - type of the node. This field is {@code null}
+ * for the root node ({@code node.object === 'document'}).
+ *
+ * @property {Immutable~Map} counters - values of all counters at this point
+ * in the document tree.
+ *
+ * @property {Immutable~List} nodes - states of counters in children of
+ * this node. This list includes only block nodes.
+ */
+export default class State extends Record(DEFAULTS) {
+    static fromJS(object) {
+        if (object instanceof State) {
+            return object
+        }
+
+        let { key, type, counters, nodes } = object
+
+        type = type || null
+
+        counters = new Map(counters)
+        nodes = Seq.Indexed(nodes)
+            .map(State.fromJS)
+            .toList()
+
+        return new State({ key, type, counters, nodes })
+    }
+}
