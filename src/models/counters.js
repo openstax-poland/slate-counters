339b5ebc3594e6951554bea9953a80da7fb24645
Krzysztof Mędrzycki <aiwenari@gmail.com>
2018-04-26 15:21:55 +0200

    Add Counters

---
+++ src/models/counters.js
@@ -0,0 +1,40 @@
+import { Record, Map, Seq } from 'immutable'
+
+import State from './state'
+import Schema from './schema'
+
+const DEFAULTS = {
+    values: new Map(),
+    document: new State(),
+    schema: new Schema({}),
+}
+
+/**
+ * Similarly to Slate's {@link Slate~Value} the purpose of this record is
+ * to keep all information about counter states in a document together.
+ *
+ * @property {Immutable~Map} values - mapping from node keys to counter values
+ * on that node. It allows retrieving counter values without traversing
+ * document tree.
+ *
+ * @property {State} document - state of the root node of the document.
+ *
+ * @property {Schema} schema - counter schema associated with this document.
+ */
+export default class Counters extends Record(DEFAULTS) {
+    static fromJS(object) {
+        if (object instanceof Counters) {
+            return object
+        }
+
+        let { values = {}, document = {}, schema = {} } = object
+
+        document = State.fromJS(document)
+        schema = Schema.fromJS(schema)
+        values = Seq.Keyed(values)
+            .map(values => new Map(values))
+            .toMap()
+
+        return new Counters({ document, schema, values })
+    }
+}
