f01ba79624dbb2e832895a6241711547be8a1c22
Krzysztof Mędrzycki <aiwenari@gmail.com>
2018-05-08 09:03:21 +0200

    Switch to Babel until Bublé supports iterators and generators

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import buble from 'rollup-plugin-buble'
+import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 
 export default {
@@ -17,12 +17,9 @@ export default {
         },
     ],
     plugins: [
-        buble({
-            target: {
-                chrome: 52,
-                firefox: 52,
-            },
-            objectAssign: 'Object.assign',
+        babel({
+            exclude: 'node_modules/**',
+            runtimeHelpers: true,
         }),
         resolve(),
     ],
@@ -31,5 +28,19 @@ export default {
         'prop-types',
         'react',
         'slate',
+        'babel-runtime/helpers/inherits',
+        'babel-runtime/helpers/classCallCheck',
+        'babel-runtime/helpers/possibleConstructorReturn',
+        'babel-runtime/core-js/object/get-prototype-of',
+        'babel-runtime/core-js/map',
+        'babel-runtime/helpers/createClass',
+        'babel-runtime/core-js/get-iterator',
+        'babel-runtime/helpers/slicedToArray',
+        'babel-runtime/helpers/extends',
+        'babel-runtime/core-js/object/entries',
+        'babel-runtime/core-js/object/freeze',
+        'babel-runtime/regenerator',
+        'babel-runtime/helpers/typeof',
+        'babel-runtime/helpers/toConsumableArray',
     ]
 }
