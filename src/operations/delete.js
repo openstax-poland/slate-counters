28b8a9f4ca59a99ffa241707540effb619b0ad34
Krzysztof Mędrzycki <aiwenari@gmail.com>
2018-11-07 15:36:06 +0100

    Add license

--- src/operations/delete.js
+++ src/operations/delete.js
@@ -1,3 +1,7 @@
+// Copyright 2018 OpenStax Poland
+// Licensed under the MIT license. See LICENSE file in the project root for
+// full license text.
+
 import { Seq } from 'immutable'
 
 import { update, updateTree } from './update'
